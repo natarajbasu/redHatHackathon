0c9688e775bdb81131b7ff09debc20d14760c1af
Nataraj Basu <nataraj.basu@in.ibm.com>
2023-05-05 09:03:00 +0530

    added Anupam's changes

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 const session = require('express-session');
+const mysql = require('mysql2');
+
 
 
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+const mySQLConnection = mysql.createConnection({
+	host: "hackathon-mysql-rhel8-hackathon2023-prometheus.mycluster-wdc04-b3c-16x64-bcd9381b2e59a32911540577d00720d7-0000.us-east.containers.appdomain.cloud",
+	port: 31490,
+	user: "user",
+	password: "pass",
+	database: "opdb"
+});
 
 app.use(session({
 	secret: 'PrometheusHackathonSessionSecret',
@@ -35,6 +45,334 @@ router.get('/register', function(req, res) {
 	//__dirname : It will resolve to your project folder.
 });
 
+router.post('/getSessionMessage', function(req, res) {
+	var sessionKeyValue = req.body.sessionKey;
+	var valueToBeRetruned = '';
+	if (sessionKeyValue == 'unAuthAccess') {
+		valueToBeRetruned = { 'message': req.session.unAuthAccess, 'type': ' Error' };
+
+	} else if (sessionKeyValue == 'firstPage') {
+
+		var message = '';
+		var registerMessage = req.session.register;
+		var passwordMessage = req.session.password;
+		var logoutMessage = req.session.logout;
+
+		if (registerMessage != undefined) {
+			message = registerMessage;
+			req.session.register = undefined;
+		}
+		if (passwordMessage != undefined) {
+			message = passwordMessage;
+			req.session.password = undefined;
+		}
+		if (logoutMessage != undefined) {
+			message = logoutMessage;
+			req.session.logout = undefined;
+		}
+		valueToBeRetruned = { 'message': message, 'type': ' Message' };
+
+		res.send(valueToBeRetruned);
+	}
+});
+router.post('/loginUser', function(req, res) {
+
+	var userName = req.body.userName;
+	var password = req.body.password;
+	var isError = false;
+	var userSearchResult = null;
+	var callStatus = null;
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			isError = true;
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+
+			res.send(callStatus);
+		}
+		mySQLConnection.query("select * from tr_traveller where username='" + userName + "' AND password='" + password + "'", function(err, result, fields) {
+			if (err) {
+				isError = true;
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue in retriving the information.' };
+
+				res.send(callStatus);
+			}
+
+			userSearchResult = result;
+			if (isError) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+			} else {
+
+				if (userSearchResult != null) {
+					if (userSearchResult.length == 1) {
+						callStatus = { 'status': 'success' };
+					} else {
+						callStatus = { 'status': 'error', 'errorMsg': 'User Name or Password does not match.' };
+					}
+				} else {
+
+				}
+
+			}
+
+
+
+			res.send(callStatus);
+		});
+	});
+
+
+});
+router.post('/searchUserDetails', function(req, res) {
+
+	var userName = req.body.userName;
+	var isError = false;
+	var userSearchResult = null;
+	var callStatus = null;
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			isError = true;
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+
+			res.send(callStatus);
+		}
+		mySQLConnection.query("select * from opdb.tr_traveller where username='" + userName + "'", function(err, result, fields) {
+			if (err) {
+				isError = true;
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue in retriving the information.' };
+
+				res.send(callStatus);
+			}
+
+			userSearchResult = result;
+			if (isError) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+			} else {
+
+				if (userSearchResult != null) {
+					if (userSearchResult.length == 1) {
+						callStatus = { 'status': 'success', 'question': userSearchResult[0].secretques, 'answer': userSearchResult[0].secretans, 'id': userSearchResult[0].traveller_id };
+					} else {
+						callStatus = { 'status': 'error', 'errorMsg': 'User Name does not exist.' };
+					}
+				} else {
+
+				}
+
+			}
+
+
+
+			res.send(callStatus);
+		});
+	});
+
+
+});
+
+router.post('/passwordChange', function(req, res) {
+	var newPassword = req.body.newPassword;
+	var id = req.body.id;
+
+	var callStatus = null;
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try again after some time.' };
+			res.send(callStatus);
+		}
+		var sql = "UPDATE opdb.tr_traveller SET password = '" + newPassword + "' WHERE traveller_id =" + id;
+		mySQLConnection.query(sql, function(err, result) {
+			if (err) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try again after some time.' };
+				res.send(callStatus);
+			}
+			if (result != undefined) {
+				if (result.affectedRows == 1) {
+					callStatus = { 'status': 'success' }
+					req.session.password = "Your password has been successfully updated.";
+					res.send(callStatus);
+				} else {
+					callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try again after some time.' };
+					res.send(callStatus);
+				}
+			} else {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try again after some time.' };
+				res.send(callStatus);
+			}
+
+		});
+	});
+
+
+});
+
+router.post('/getUserInterestList', function(req, res) {
+
+
+	var isError = false;
+	var userInterestsList = null;
+	var callStatus = null;
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			isError = true;
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+
+			res.send(callStatus);
+		}
+		mySQLConnection.query("select * from opdb.tr_list_interests", function(err, result, fields) {
+			if (err) {
+				isError = true;
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue in retriving the information.' };
+
+				res.send(callStatus);
+			}
+
+			userInterestsList = result;
+			if (isError) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+			} else {
+
+
+				callStatus = { 'status': 'success', 'interests': userInterestsList };
+			}
+
+
+
+			res.send(callStatus);
+		});
+	});
+
+
+});
+
+router.post('/avaiableUserId', function(req, res) {
+
+
+	var isError = false;
+	var avaiableUserId = null;
+	var callStatus = null;
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			isError = true;
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+
+			res.send(callStatus);
+		}
+		mySQLConnection.query("SELECT MAX(traveller_id)+1 AS AVAILABLEID FROM opdb.tr_traveller", function(err, result, fields) {
+			if (err) {
+				isError = true;
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue in retriving the information.' };
+
+				res.send(callStatus);
+			}
+
+			avaiableUserId = result;
+			if (isError) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.' };
+			} else {
+
+
+				callStatus = { 'status': 'success', 'avaiableUserId': avaiableUserId };
+			}
+
+
+
+			res.send(callStatus);
+		});
+	});
+
+
+});
+router.post('/registerUser', function(req, res) {
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	var userName = req.body.userName;
+	var password = req.body.password;
+	var secretQues = req.body.secretQues;
+	var sectAnswer = req.body.sectAnswer;
+	var userGender = req.body.userGender;
+	var dateOfBirth = req.body.dateOfBirth;
+	var avaiableUserId = req.body.avaiableUserId;
+	var userAlreadyExsits = false;
+	var callStatus = null;
+
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try after some time' };
+
+			res.send(callStatus);
+		}
+        var dateOfBirthDate=new Date(dateOfBirth);
+        var dateOfBirthString=dateOfBirthDate.getFullYear()+"-"+(dateOfBirthDate.getMonth()+1)+"-"+dateOfBirthDate.getDate();
+       
+		var sql = "INSERT INTO opdb.tr_traveller VALUES (" + avaiableUserId + ",'" + firstName + "','" + lastName + "','" + userName + "','" + password + "','" + secretQues + "','" + sectAnswer + "','" + userGender + "','" + dateOfBirthString + "')";
+		
+		mySQLConnection.query(sql, function(err, result) {
+			
+			if (err) {
+				if (err.code = 'ER_DUP_ENTRY') {
+					callStatus = { 'status': 'error', 'errorMsg': 'The user name already exists in the system.Please use another user name' };
+				} else {
+					callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try after some time.' };
+				}
+
+
+
+				res.send(callStatus);
+
+			} else {
+				callStatus = { 'status': 'success' };
+
+				res.send(callStatus);
+			}
+
+		});
+	});
+
+});
+
+router.post('/registerInterests', function(req, res) {
+	var avaiableUserId = req.body.avaiableUserId;
+	var selectedInterest = req.body.selectedInterest;
+
+	var callStatus = null;
+
+	mySQLConnection.connect(function(err) {
+		if (err) {
+			callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try after some time' };
+
+			res.send(callStatus);
+		}
+
+		var sql = "";
+		var insertData = "";
+		for (var i = 0; i < selectedInterest.length; i++) {
+			if (insertData == "") {
+				insertData = insertData + "(" + avaiableUserId + ",'" + selectedInterest[i] + "')";
+			} else {
+				insertData = insertData + ", (" + avaiableUserId + ",'" + selectedInterest[i] + "')";
+			}
+
+		}
+		sql = "INSERT INTO opdb.tr_traveller_interest VALUES " + insertData;
+        console.log(sql); 
+		mySQLConnection.query(sql, function(err, result) {
+            console.log(err);
+            console.log(result);     
+			if (err) {
+				callStatus = { 'status': 'error', 'errorMsg': 'There is some technical issue.Please try after some time.' };
+				res.send(callStatus);
+
+			} else {
+				req.session.register = "You have successfully register the user.";
+				callStatus = { 'status': 'success' };
+
+				res.send(callStatus);
+			}
+
+		});
+	});
+
+});
 //add the router
 app.use('/', router);
 app.use('/public', express.static(__dirname + '/public'));
--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -21,19 +21,245 @@ prometheusHackathonModuleApp.config(function($routeProvider) {
 
 
 prometheusHackathonModuleApp.controller("welcomePageController", function($scope, $http, $window) {
+	$http.post('/getSessionMessage', {
+		'sessionKey': 'unAuthAccess'
+
+	}).then(function(response) {
+		var dataRetruned = response.data;
+
+		if (dataRetruned.type.trim() == 'Error') {
+
+			$scope.errorMessage = dataRetruned.message;
+
+		}
+
+	});
+
+	$http.post('/getSessionMessage', {
+		'sessionKey': 'firstPage'
+
+	}).then(function(response) {
+		var dataRetruned = response.data;
+
+		if (dataRetruned.type.trim() == 'Message') {
+
+			$scope.infoMessage = dataRetruned.message;
+		}
+
+	});
+
+	$scope.submitMutualFundForm = function() {
+		$http.post('/loginUser', {
+			userName: $scope.user.userName,
+			password: $scope.user.password
+		}).then(function(response) {
+			console.log(response);
+			var callStatus = response.data;
+			if (callStatus.status == 'success') {
 
 
+				$window.location.href = '/loginPage';
+			} else {
+				$scope.errorMessage = callStatus.errorMsg;
+				$window.location.replace = '/';
+			}
 
-});
-
-prometheusHackathonModuleApp.controller("homePageController", function($scope, $http, $window) {
-
-});
-prometheusHackathonModuleApp.controller("registrationPageController", function($scope, $http, $window) {
-
+		});
+	}
 
 });
 prometheusHackathonModuleApp.controller("forgetPasswordPageController", function($scope, $http, $window) {
 
+	$scope.searchUserDetails = function() {
+		$scope.errorMessage = '';
+		$http.post('/searchUserDetails', {
+			userName: $scope.forgetPassword.userName
+		}).then(function(response) {
+
+			$scope.searchResultDeatils = response.data;
+			if ($scope.searchResultDeatils.status == 'success') {
+				$scope.secretQuestion = $scope.searchResultDeatils.question;
+				$scope.forgetPassword.secretQuesSec = true;
+				$scope.forgetPasswordUserNameDisable = true;
+				$window.location.relace = '/forgetPassword';
+			} else {
+				$scope.errorMessage = $scope.searchResultDeatils.errorMsg;
+				$window.location.relace = '/forgetPassword';
+			}
+
+
+
+
+		});
+
+	};
+	$scope.submitAnswer = function() {
+		var secretQuesAns = $scope.forgetPassword.secretQuesAns;
+
+		if ($scope.searchResultDeatils.answer.toLowerCase() == secretQuesAns.toLowerCase()) {
+
+			$scope.forgetPassword.changePasswordSec = true;
+			$scope.forgetPasswordSecretAnswerDisable = true;
+			$window.location.relace = '/forgetPassword';
+
+
+		} else {
+			$scope.errorMessage = 'Your Secret Answer is not Matching.Please enter valid answer';
+			$window.location.relace = '/forgetPassword';
+		}
+
+
+	};
+	$scope.changePassword = function() {
+		var newPassword = $scope.forgetPassword.newPassword.trim();
+		var confirmNewPassword = $scope.forgetPassword.confirmNewPassword.trim();
+
+		$scope.errorMessage = '';
+		if (newPassword != confirmNewPassword) {
+			$scope.errorMessage = 'New Password and Confirm New Password are not matching.Please try again.';
+			$window.location.relace = '/forgetPassword';
+
+		} else {
+
+			$http.post('/passwordChange', {
+				newPassword: newPassword,
+				id: $scope.searchResultDeatils.id
+			}).then(function(response) {
+
+				if (response.data.status == 'success') {
+
+					$window.location.href = '/';
+
+				} else {
+					$scope.errorMessage = response.data.errorMsg;
+					$window.location.relace = '/forgetPassword';
+				}
+
+			}).catch(function(error) {
+				$scope.errorMessage = 'There is some issue with the Password Reset Process. Please try after some time';
+				$window.location.relace = '/forgetPassword';
+
+
+			});
+
+
+		}
+
+
+	};
 
 });
+prometheusHackathonModuleApp.controller("registrationPageController", function($scope, $http, $window) {
+	$scope.selectedInterest = [];
+	$scope.toggleSelection = function toggleSelection(selectedInterest) {
+		var idx = $scope.selectedInterest.indexOf(selectedInterest);
+
+		// Is currently selected
+		if (idx > -1) {
+			$scope.selectedInterest.splice(idx, 1);
+		}
+
+		// Is newly selected
+		else {
+			$scope.selectedInterest.push(selectedInterest.interest);
+		}
+		console.log($scope.selectedInterest);
+	};
+
+	$http.post('/getUserInterestList', {
+
+	}).then(function(response) {
+
+		if (response.data.status == 'success') {
+
+			$scope.userInterests = response.data.interests;
+
+		}
+
+	});
+	$http.post('/avaiableUserId', {
+
+	}).then(function(response) {
+
+		if (response.data.status == 'success') {
+
+			$scope.avaiableUserId = response.data.avaiableUserId[0].AVAILABLEID;
+
+		}
+
+	});
+	$scope.registerUserForm = function() {
+		$scope.errorMessage = '';
+		$scope.infoMessage = '';
+		var password = $scope.register.password.trim();
+		var confirmPassword = $scope.register.confirmPassword.trim();
+		if (password != confirmPassword) {
+			$scope.errorMessage = 'Password and ReEntered Password are not matching. ';
+			$window.location.relace = '/register';
+
+		} else {
+			$http.post('/registerUser', {
+				firstName: $scope.register.firstName,
+				lastName: $scope.register.lastName,
+				userName: $scope.register.userName,
+				password: $scope.register.password,
+				secretQues: $scope.register.secretQues,
+				sectAnswer: $scope.register.sectAnswer,
+				userGender: $scope.register.userGender,
+				dateOfBirth: $scope.register.dateOfBirth,
+				avaiableUserId: $scope.avaiableUserId
+
+			}).then(function(response) {
+				var responseStatus = response.data.status;
+				if (responseStatus == 'error') {
+					$scope.errorMessage = response.data.errorMsg;
+					$window.location.relace = '/register';
+				} else {
+					console.log($scope.register);
+					console.log($scope.selectedInterest);
+					$http.post('/registerInterests', {
+						avaiableUserId: $scope.avaiableUserId,
+						selectedInterest: $scope.selectedInterest
+					}).then(function(responsePref) {
+						var responseStatus= responsePref.data.status;
+					
+						
+						if (responseStatus == 'error') {
+							$scope.errorMessage = responsePref.data.errorMsg;
+							$window.location.relace = '/register';
+						} else {
+							
+							
+							$window.location.href = '/';
+
+						}
+
+
+
+					}).catch(function(errorPref) {
+						$scope.errorMessage = error.data.errorMessage;
+						$window.location.relace = '/register';
+
+
+					});
+
+				}
+
+
+			}).catch(function(error) {
+				$scope.errorMessage = error.data.errorMessage;
+				$window.location.relace = '/register';
+
+
+			});
+		}
+
+
+	};
+
+
+});
+prometheusHackathonModuleApp.controller("homePageController", function($scope, $http, $window) {
+
+});
+
