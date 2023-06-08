c90692d173fae866084670cc844706fd93036aea
Nataraj Basu <nataraj.basu@in.ibm.com>
2023-06-08 15:13:39 +0530

    added test details

--- test/testapp.js
+++ test/testapp.js
@@ -11,7 +11,7 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe('/GET', () => {
-    it('it should GET the user details', (done) => {
+    it('it should GET the main page', (done) => {
     chai.request(App)
         .get('/')
         .end((err, res) => {
@@ -46,8 +46,33 @@ describe('/GET register', () => {
     });
 });
 
+describe('/GET forgetPassword', () => {
+    it('it should GET the login page', (done) => {
+    chai.request(App)
+        .get('/forgetPassword')
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST getSessionMessage', () => {
+    it('it should Return the session message', (done) => {
+    chai.request(App)
+        .post('/getSessionMessage')
+        .send({"sessionKey":"firstPageInfo"})
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
 describe('/POST searchUserDetails', () => {
-    it('it should GET the user details', (done) => {
+    it('it should Return the user details', (done) => {
     chai.request(App)
         .post('/searchUserDetails')
         .send({"userName":"nbasuibm"})
@@ -66,9 +91,9 @@ describe('/POST searchUserDetails', () => {
 });
 
 describe('/POST loginUser', () => {
-    it('it should GET the user details', (done) => {
+    it('it should Rreturn the login status', (done) => {
     var userName = "nbasuibm"
-    var password = "ibm123";
+    var password = "abc123";
     chai.request(App)
         .post('/loginUser')
         .send({
@@ -84,3 +109,164 @@ describe('/POST loginUser', () => {
         });
     });
 });
+
+// describe('/POST passwordChange', () => {
+//     it('it should Return the POST status', (done) => {
+//     chai.request(App)
+//         .post('/passwordChange')
+//         .send({
+//             "newPassword":"abc123",
+//             "id":"nbasuibm"})
+//         .end((err, res) => {
+//                 res.should.have.status(200);
+//                 // res.body.should.be.a('object');
+//             done();
+//         });
+//     });
+// });
+
+describe('/POST getUserInterestList', () => {
+    it('it should Return the a list of users interest', (done) => {
+    chai.request(App)
+        .post('/getUserInterestList')
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST avaiableUserId', () => {
+    it('it should Return a list of avaiable User Ids', (done) => {
+    chai.request(App)
+        .post('/avaiableUserId')
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST registerUser', () => {
+    it('it should Return the user registration status', (done) => {
+    chai.request(App)
+        .post('/registerUser')
+        .send({
+            "firstName": "fName",
+            "lastName": "lName",
+            "userName": "uName",
+            "password": "pass123",
+            "secretQues": "secQ",
+            "sectAnswer": "secA",
+            "userGender": "M",
+            "dateOfBirth": "01/01/1991",
+            "avaiableUserId": "avUId"
+        })
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST registerInterests', () => {
+    it('it should Return the registerInterests status', (done) => {
+    chai.request(App)
+        .post('/registerInterests')
+        .send({
+            "avaiableUserId":"firstPageInfo",
+            "selectedInterest":"firstPageInfo"
+            })
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST populateLoginData', () => {
+    it('it should Return the populate Login Data status', (done) => {
+    chai.request(App)
+        .post('/populateLoginData')
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST getRecomondation', () => {
+    it('it should Return the recommendations', (done) => {
+    chai.request(App)
+        .post('/getRecomondation')
+        .send({"recomondationList":"firstPageInfo"})
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST logoutUser', () => {
+    it('it should Return the logout status', (done) => {
+    chai.request(App)
+        .post('/logoutUser')
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST openGraph', () => {
+    it('it should Return the openGraph status', (done) => {
+    chai.request(App)
+        .post('/openGraph')
+        .send({
+            "graphInput": "firstPageInfo",
+            "graphType": "fullGraph" 
+        })
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST updateUserInterest', () => {
+    it('it should Return the updateUserInterest status', (done) => {
+    chai.request(App)
+        .post('/updateUserInterest')
+        .send({"loggedInUserId":"nbasuibm"})
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+describe('/POST updateUserTravel', () => {
+    it('it should Return the updateUserTravel status', (done) => {
+    chai.request(App)
+        .post('/updateUserTravel')
+        .send({
+            "loggedInUserId": "nbasuibm",
+            "locationId": "10"
+        })
+        .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
